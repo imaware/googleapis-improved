7ab6c3b907d0ebb4fa4ac92aea9dbeb95596efe9
Christopher Manzi <chris@imaware.health>
2022-02-23 10:05:03 -0600

    fix: fixes release workflow

--- src/__tests__/env/integration.ts
+++ src/__tests__/env/integration.ts
@@ -2,3 +2,5 @@ process.env.GCP_PROJECT = 'imaware-test';
 process.env.GCP_REGION = 'us-central1';
 process.env.TEST_FHIR_DATASET = 'test-dataset';
 process.env.TEST_FHIR_STORE_ID = 'googleapis-improved-integration';
+process.env.CREATE_DATA = '1';
+process.env.CLEAN_DATA = '1';
